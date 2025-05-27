688a79b13d74dc389935f41e30583e77e9f5717d
hemanthreddy171 <gahredd2@newhaven.edu>
2025-05-27 09:00:40 -0400

    added seperate login and added dashboard for the doctor

---
+++ AppointPro/Migrations/20250526151724_addednormallogin.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppointPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class addednormallogin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Appointments_AppointmentId",
+                table: "Payments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Appointments_AppointmentId",
+                table: "Payments",
+                column: "AppointmentId",
+                principalTable: "Appointments",
+                principalColumn: "AppointmentId",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Appointments_AppointmentId",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Appointments_AppointmentId",
+                table: "Payments",
+                column: "AppointmentId",
+                principalTable: "Appointments",
+                principalColumn: "AppointmentId");
+        }
+    }
+}
---
+++ AppointPro/Models/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace AppointPro.Models
+{
+    using System.ComponentModel.DataAnnotations;
+        public class LoginViewModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Required]
+            public UserRole Role { get; set; }
+        }
+
+        public class RegisterViewModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            public UserRole Role { get; set; }
+        }
+    }
